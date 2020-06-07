59e72b8e4565a73e53c056a0285da9cc801f51b9
tobiaswahyudi <tobi8800@gmail.com>
2020-06-07 01:50:42 -0400

    Realize that the name is wrong

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/medium-rare'),
+      dir: require('path').join(__dirname, './coverage/medium-well'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'medium-rare'`, () => {
+  it(`should have as title 'medium-well'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('medium-rare');
+    expect(app.title).toEqual('medium-well');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('medium-rare app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('medium-well app is running!');
   });
 });
