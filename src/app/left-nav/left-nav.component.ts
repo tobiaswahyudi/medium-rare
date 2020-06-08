1bc06e25857cb501ed6b932401c5fd89f61e284d
tobiaswahyudi <tobi8800@gmail.com>
2020-06-07 22:49:55 -0400

    Create left nav content and styling

--- src/app/left-nav/left-nav.component.ts
+++ src/app/left-nav/left-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import FirebaseService from '../firebase/firebase.service';
 
 @Component({
   selector: 'app-left-nav',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public firebaseService: FirebaseService) { }
 
   ngOnInit(): void {
   }
