1fd07b00f0b5bee20ff77b6d649f7c30d7946381
tobiaswahyudi <tobi8800@gmail.com>
2020-06-07 22:05:48 -0400

    Fix document type name and add firestore listener

--- src/app/firebase/firestore.service.ts
+++ src/app/firebase/firestore.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import FirebaseService from './firebase.service';
-import { UserCollection } from '../types';
+import { UserCollection, MWDocument } from '../types';
 import { Observable } from 'rxjs';
-import { docData } from 'rxfire/firestore';
+import { docData, collectionData } from 'rxfire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
+
+  documents: MWDocument[];
+
   constructor(private firebaseService: FirebaseService) { }
 
   public getUser(): Observable<UserCollection> {
@@ -16,5 +19,14 @@ export class FirestoreService {
       throw new Error('User must be logged in.');
     }
     return docData(this.firebaseService.firestoreRef.doc(user.uid));
+  }
+
+  public updateDocs(): void {
+    const user = this.firebaseService.user;
+    if (!user) {
+      throw new Error('User must be logged in.');
+    }
+    collectionData(this.firebaseService.firestoreRef.doc(user.uid).collection('documents'))
+    .subscribe(console.log);
   }
 }
