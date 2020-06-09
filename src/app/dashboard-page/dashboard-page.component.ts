acd8ba18bf575ea1b587fc8dc62083c3eedbcca3
tobiaswahyudi <tobi8800@gmail.com>
2020-06-09 02:47:00 -0400

    Implement searching

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
 import FirebaseService from '../firebase/firebase.service';
 import { FirestoreService } from '../firebase/firestore.service';
 import { MWDocument } from '../types';
@@ -9,17 +9,19 @@ import { Router } from '@angular/router';
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, AfterContentChecked {
+
+  search: string;
+  filtered: MWDocument[];
 
   constructor(
     public firebaseService: FirebaseService,
     public firestoreService: FirestoreService,
     private router: Router
-  ) {}
-
-  docs: MWDocument[];
+  ) { }
 
   ngOnInit(): void {
+    this.searchChange = this.searchChange.bind(this);
     // Should probably protect from router rather than the components.
     // Auth takes an extra event loop (?) so it would be useless either way
     // TODO: Find actual way to do route protection
@@ -27,5 +29,15 @@ export class DashboardPageComponent implements OnInit {
       this.router.navigateByUrl('/');
     }
     this.firestoreService.createOnFirstTime();
+  }
+
+  ngAfterContentChecked(): void {
+    if (!this.filtered) {
+      this.filtered = this.firestoreService.docs;
+    }
+  }
+
+  searchChange(query: string): void {
+    this.filtered = this.firestoreService.docs.filter(doc => doc.title.toLocaleLowerCase().startsWith(query.toLocaleLowerCase()));
   }
 }
